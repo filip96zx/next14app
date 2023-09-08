931959e10a32e8defe9e4f5577fb00a04ad2867c
Filip Cudny <filip96zx@gmail.com>
2023-09-08 21:14:15 +0200

    Dodanie test-id

--- src/app/ui/organisms/ProductList.tsx
+++ src/app/ui/organisms/ProductList.tsx
@@ -7,7 +7,10 @@ interface IProps {
 
 export const ProductList = ({ products }: IProps) => {
 	return (
-		<ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<ul
+			className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
