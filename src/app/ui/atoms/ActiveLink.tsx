30b50d572eb2344f75a7b4b0bd8c9c32dbc57af1
Filip Cudny <filip96zx@gmail.com>
2023-09-10 23:17:11 +0200

    Dodanie propsów do przekazana nazw klas

--- src/app/ui/atoms/ActiveLink.tsx
+++ src/app/ui/atoms/ActiveLink.tsx
@@ -1,26 +1,27 @@
 "use client";
 
 import clsx from "clsx";
-import { type Route } from "next";
 
-import Link from "next/link";
+import Link, { type LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 
-type TProps<T extends string> = {
-	href: Route<T>;
+type TProps<T> = {
+	href: LinkProps<T>["href"];
 	children: React.ReactNode;
+	className?: string;
+	activeClassName?: string;
 };
 
-export function ActiveLink<T extends string>({ children, href }: TProps<T>) {
+export function ActiveLink<T>({
+	children,
+	href,
+	className = "mt-2 text-blue-500 hover:text-blue-700",
+	activeClassName = "underline",
+}: TProps<T>) {
 	const currentPath = usePathname();
 	const isActive = currentPath === href;
 	return (
-		<Link
-			href={href}
-			className={clsx("mt-2` text-blue-500 hover:text-blue-700", {
-				underline: isActive,
-			})}
-		>
+		<Link href={href} className={clsx(className, isActive && activeClassName)}>
 			{children}
 		</Link>
 	);
