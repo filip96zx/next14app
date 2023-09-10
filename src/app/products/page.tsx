e3ed7dc365c600175ef8b7a15316955f26493071
Filip Cudny <filip96zx@gmail.com>
2023-09-10 12:15:24 +0200

    Zmiana nazwy funkcji

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -44,10 +44,6 @@ const products: Array<TProduct> = [
 	},
 ];
 
-export default function Home() {
-	return (
-		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-			<ProductList products={products} />
-		</section>
-	);
+export default function ProductsPage() {
+	return <ProductList products={products} />;
 }
