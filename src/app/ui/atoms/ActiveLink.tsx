438b59eb44664c7d2e0808cc3e60308790979a5c
Filip Cudny <filip96zx@gmail.com>
2023-09-10 12:15:47 +0200

    Implementacja active link

---
+++ src/app/ui/atoms/ActiveLink.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import clsx from "clsx";
+
+import Link, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
+
+interface IProps<T> {
+	href: LinkProps<T>["href"];
+	children: React.ReactNode;
+}
+
+export function ActiveLink<T>({ children, href }: IProps<T>) {
+	const currentPath = usePathname();
+	const isActive = currentPath === href;
+	return (
+		<Link
+			href={href}
+			className={clsx("mt-2` text-blue-500 hover:text-blue-700", {
+				underline: isActive,
+			})}
+		>
+			{children}
+		</Link>
+	);
+}
