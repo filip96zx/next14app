c69803664879be84cf9b6bb2d4f97a237f097fee
Filip Cudny <filip96zx@gmail.com>
2023-09-10 23:32:47 +0200

    Dodanego opcjonalnego dopasowania scieżki

--- src/app/ui/atoms/ActiveLink.tsx
+++ src/app/ui/atoms/ActiveLink.tsx
@@ -1,25 +1,28 @@
 "use client";
 
 import clsx from "clsx";
+import { type Route } from "next";
 
-import Link, { type LinkProps } from "next/link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-type TProps<T> = {
-	href: LinkProps<T>["href"];
+type TProps<T extends string> = {
+	href: Route<T>;
+	exact?: boolean;
 	children: React.ReactNode;
 	className?: string;
 	activeClassName?: string;
 };
 
-export function ActiveLink<T>({
+export function ActiveLink<T extends string>({
 	children,
 	href,
+	exact,
 	className = "mt-2 text-blue-500 hover:text-blue-700",
 	activeClassName = "underline",
 }: TProps<T>) {
 	const currentPath = usePathname();
-	const isActive = currentPath === href;
+	const isActive = exact ? currentPath === href : currentPath.startsWith(href);
 	return (
 		<Link href={href} className={clsx(className, isActive && activeClassName)}>
 			{children}
