a8653bf180de27d335490105dbd92782ac68bfd6
Filip Cudny <filip96zx@gmail.com>
2023-09-11 00:12:29 +0200

    Implementacja api

---
+++ src/app/api/products.ts
@@ -0,0 +1,46 @@
+import { type TProduct } from "@/app/types";
+
+const apiURL = "https://naszsklep-api.vercel.app/api";
+
+export interface ProductDTO {
+	id: string;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	rating: Rating;
+	image: string;
+	longDescription: string;
+}
+
+export interface Rating {
+	rate: number;
+	count: number;
+}
+
+const parseProductDTOToTProduct = (product: ProductDTO): TProduct => {
+	return {
+		id: product.id,
+		name: product.title,
+		category: product.category,
+		price: product.price,
+		image: {
+			src: product.image,
+			alt: product.title,
+		},
+		description: product.description,
+	};
+};
+
+export const getProductById = async (id: string): Promise<TProduct> => {
+	const response = await fetch(`${apiURL}/products/${id}`);
+	const product = (await response.json()) as ProductDTO;
+	return parseProductDTOToTProduct(product);
+};
+
+export const getProducts = async (): Promise<TProduct[]> => {
+  const response = await fetch(`${apiURL}/products`);
+  const products = (await response.json()) as ProductDTO[];
+  return products.map(parseProductDTOToTProduct);
+};
+
