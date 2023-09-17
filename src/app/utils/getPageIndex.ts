b4b9f930f0734e9629eab5dbd1550a0d2869a88e
Filip Cudny <filip96zx@gmail.com>
2023-09-17 11:20:30 +0200

    refactor: Usunięcie nieużywanego utilsa

--- src/app/utils/getPageIndex.ts
+++
@@ -1,9 +0,0 @@
-export const getPageIndex = ({ pageNumber, totalPages }: { pageNumber: number; totalPages: number }) => {
-	if (pageNumber > totalPages) {
-		return totalPages - 1;
-	}
-	if (pageNumber < 1) {
-		return 0;
-	}
-	return pageNumber - 1;
-};
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,2 +1,1 @@
 export { parseMoney } from "./parseMoney";
-export { getPageIndex } from "./getPageIndex";
