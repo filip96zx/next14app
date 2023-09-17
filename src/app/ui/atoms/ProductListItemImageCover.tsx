35d55f29509f676ba770c2103ceeec9b73288ca0
Filip Cudny <filip96zx@gmail.com>
2023-09-17 11:03:22 +0200

    Poprawienie literówki

--- src/app/ui/atoms/ProductListItemImageCover.tsx
+++ src/app/ui/atoms/ProductListItemImageCover.tsx
@@ -4,7 +4,7 @@ export const ProductListItemImageCover = ({ alt, src }: TImage) => {
 	return (
 		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-100">
 			<img
-				className="obbject-center h-full w-full object-cover p-4 transition-transform hover:scale-105"
+				className="object-center h-full w-full object-cover p-4 transition-transform hover:scale-105"
 				width={320}
 				height={320}
 				src={src}
