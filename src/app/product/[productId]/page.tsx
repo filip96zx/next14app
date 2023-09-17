abb99e9798251b89439bbb05a80904be27395484
Filip Cudny <filip96zx@gmail.com>
2023-09-17 19:34:30 +0200

    feat: Dynamiczne generowanie meta danych

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,6 +1,25 @@
 import Link from "next/link";
 import { getProductById } from "@/app/api/products";
 import { ProductCard } from "@/app/ui/molecules/ProductCard";
+import { getMetadataTitle } from "@/app/utils";
+
+export const generateMetadata = async ({ params }: { params: { productId: string } }) => {
+	const product = await getProductById(params.productId);
+	return {
+		title: getMetadataTitle(product.name),
+		description: product.description,
+		openGraph: {
+			title: getMetadataTitle(product.name),
+			description: product.description,
+			images: [
+				{
+					url: product.image.src,
+					alt: product.name,
+				},
+			],
+		},
+	};
+};
 
 interface IProps {
 	params: { productId: string };
