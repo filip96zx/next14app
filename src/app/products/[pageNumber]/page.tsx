73ae7a19be9c833507b2075351375b2f47393b26
Filip Cudny <filip96zx@gmail.com>
2023-09-22 21:55:06 +0200

    chore: Dodanie generateStaticParams

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -1,5 +1,16 @@
 import { getProductsList } from "@/app/api/products";
+import { LIST_PAGE_SIZE } from "@/app/constants";
 import PaginatedProductList from "@/app/ui/organisms/PaginatedProductList";
+
+export async function generateStaticParams() {
+	const { totalElements } = await getProductsList({
+		page: 1,
+		pageSize: 1,
+	});
+	return Array.from({ length: Math.ceil(totalElements / LIST_PAGE_SIZE) }, (_, i) => ({
+		pageNumber: (i + 1).toString(),
+	}));
+}
 
 type TProps = {
 	params: { pageNumber: string };
