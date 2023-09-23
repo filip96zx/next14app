e20a1df555d75029d22c58c0012eb1158b478620
Filip Cudny <filip96zx@gmail.com>
2023-09-23 16:12:04 +0200

    Wydzielenie paginatedLIst

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -3,3 +3,5 @@ export type ListResponse<T = any> = {
 	content: Array<T>;
 	totalElements: number;
 } & { [key: string]: unknown };
+
+export type OmitKeys<T, TKey extends keyof T> = Omit<T, TKey>;
--- src/app/ui/organisms/product-list/PaginatedProductList.tsx
+++ src/app/ui/organisms/product-list/PaginatedProductList.tsx
@@ -1,56 +1,19 @@
-import { redirect } from "next/navigation";
-import { type Route } from "next";
 import { ProductList } from "./shared/ProductList";
-import { Pagination } from "@/app/ui/molecules/Pagination";
-import { type ProductListItemFragment } from "@/gql/graphql";
-import { LIST_PAGE_SIZE } from "@/app/constants";
-import { type ListResponse } from "@/app/models";
+import { type OmitKeys } from "@/app/models";
+import {
+	PaginatedList,
+	type TPaginatedListProps,
+} from "@/app/ui/organisms/product-list/shared/PaginatedList";
 
-type TQuery<TParams> = (params: TParams) => Promise<ListResponse<ProductListItemFragment>>;
-
-type TProps<TParams> = {
-	getListQuery: TQuery<TParams>;
-	params: TParams;
-	route: Route;
-	goBackParams: string;
-};
-
-export const getPaginationParams = ({
-	pageNumber,
-	pageSize = LIST_PAGE_SIZE,
-}: {
-	pageNumber: string;
-	pageSize?: number;
-}) => ({ skip: (parseInt(pageNumber) - 1) * pageSize, first: pageSize });
-
-export async function PaginatedProductList<TParams extends { skip: number; first: number }>({
-	params,
-	route,
-	getListQuery,
-	goBackParams,
-}: TProps<TParams>) {
-	const pageIndex = params.skip / params.first;
-	const isPageNumberValid = !isNaN(params.first) && pageIndex >= 0;
-	if (!isPageNumberValid) {
-		redirect(`${route}/1`);
-	}
-
-	const { content: products, totalElements } = await getListQuery(params);
-
-	const totalPages = Math.ceil(totalElements / params.first);
-
-	if (pageIndex > totalPages) {
-		redirect(`${route}/${totalPages}`);
-	}
-
-	const pageNumber = pageIndex + 1;
-
+export async function PaginatedProductList<TParams extends { skip: number; first: number }>(
+	props: OmitKeys<TPaginatedListProps<TParams>, "renderList">,
+) {
 	return (
-		<div>
-			<ProductList products={products} goBackParams={goBackParams || pageNumber} />
-			<div className="my-4 flex justify-center">
-				<Pagination page={pageNumber} totalPages={totalPages} route={route} />
-			</div>
-		</div>
+		<PaginatedList
+			{...props}
+			renderList={({ content, goBackParams }) => (
+				<ProductList products={content} goBackParams={goBackParams} />
+			)}
+		/>
 	);
 }
