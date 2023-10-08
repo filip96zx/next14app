6bdff9d849843b6c4625b93fd1236b8c13dbae7a
Filip Cudny <filip96zx@gmail.com>
2023-10-08 15:11:58 +0200

    Poprawienie styli UI

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -12,43 +12,46 @@ export default async function CartPage() {
 		redirect("/");
 	}
 	const totalAmount = cart.items.reduce((acc, { price, quantity }) => acc + price * quantity, 0);
+	const isEmpty = cart.items.length === 0;
 	return (
 		<div>
 			<PageHeader>Cart</PageHeader>
 
-			<div className="flex flex-col gap-10">
-				<table className="table-fixed text-center">
-					<thead>
-						<tr>
-							<th>Product</th>
-							<th>Price</th>
-							<th>Quantity</th>
-							<th>Total</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{cart?.items?.map((i) => {
-							return (
-								<tr key={i.variantName + i.name}>
-									<td>{`${i.name} (${i.variantName})`}</td>
-									<td>{parseMoney(i.price)}</td>
-									<td>{i.quantity}</td>
-									<td>{parseMoney(i.price * i.quantity)}</td>
-									<td className="float-right">
-										<CartProductActions orderItem={i} />
-									</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
-				<div className="flex flex-row-reverse">
-					<Link href="/payment">
-						<Button variant="primary">{`Go checkout ${parseMoney(totalAmount)}`}</Button>
-					</Link>
+			{isEmpty ? (
+				<section>Cart is empty.</section>
+			) : (
+				<div className="flex flex-col gap-10">
+					<table className="table-fixed text-center">
+						<thead>
+							<tr>
+								<th>Product</th>
+								<th>Price</th>
+								<th>Total</th>
+								<th className="w-0">Quantity</th>
+							</tr>
+						</thead>
+						<tbody>
+							{cart?.items?.map((i) => {
+								return (
+									<tr key={i.variantName + i.name}>
+										<td>{`${i.name} (${i.variantName})`}</td>
+										<td>{parseMoney(i.price)}</td>
+										<td>{parseMoney(i.price * i.quantity)}</td>
+										<td className="float-right w-fit">
+											<CartProductActions orderItem={i} />
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+					<div className="flex flex-row-reverse">
+						<Link href="/payment">
+							<Button variant="primary">{`Go checkout ${parseMoney(totalAmount)}`}</Button>
+						</Link>
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
