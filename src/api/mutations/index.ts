12bb803ad661ad414bb15730de259f198d203b72
Filip Cudny <filip96zx@gmail.com>
2023-10-08 14:09:47 +0200

    Implementacja mutacji

--- src/api/mutations/index.ts
+++ src/api/mutations/index.ts
@@ -1,4 +1,5 @@
 export { createCart } from "./createCart";
 export { addToCart } from "./addToCart";
 export { updateCart } from "./updateCart";
+export { updateOrderStatus } from "./updateOrderStatus";
 export { updateOrderItem } from "./updateOrderItem";
