86cdccaeda7469cb399b81db6de7956064831a49
Filip Cudny <filip96zx@gmail.com>
2023-10-08 23:50:31 +0200

    dodanie test id

--- src/app/ui/atoms/ProductListItemDescription.tsx
+++ src/app/ui/atoms/ProductListItemDescription.tsx
@@ -23,7 +23,7 @@ export const ProductListItemDescription = ({
 				</p>
 				<div className="flex items-center justify-center gap-3">
 					<Rating rating={averageRating} />
-					<span className="text-sm text-gray-500" data-testid="">
+					<span className="text-sm text-gray-500" data-testid="product-rating">
 						{averageRating.toFixed(2)}
 					</span>
 				</div>
