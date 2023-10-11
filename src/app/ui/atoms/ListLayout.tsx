efa146b69de41eee0a8fc73aeb3f4678d60578f3
Filip Cudny <filip96zx@gmail.com>
2023-10-11 21:22:28 +0200

    Poprawienie atrybutu testid

--- src/app/ui/atoms/ListLayout.tsx
+++ src/app/ui/atoms/ListLayout.tsx
@@ -6,7 +6,7 @@ type TProps = {
 export const ListLayout = ({ children, dataTestid }: TProps) => {
 	return (
 		<ul
-			data-test-id={dataTestid}
+			data-testid={dataTestid}
 			className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
 		>
 			{children}
