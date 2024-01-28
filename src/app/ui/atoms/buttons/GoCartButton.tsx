e34fe6fe44b1833262b345292a1d571a2b11a5a3
Filip Cudny <filip96zx@gmail.com>
2024-01-28 10:44:09 +0100

    Suspense add

--- src/app/ui/atoms/buttons/GoCartButton.tsx
+++ src/app/ui/atoms/buttons/GoCartButton.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import { ShoppingCartIcon } from "lucide-react";
 import Link from "next/link";
-import { Suspense } from "react";
 import { getCartTotalItemsByCookiesCartId } from "@/app/services/cart.service";
 
 export const GoCartButton = async () => {
@@ -15,9 +14,8 @@ export const GoCartButton = async () => {
 			{...(isCartDisabled && { "aria-disabled": true })}
 		>
 			<ShoppingCartIcon className="inline-block" />
-			<Suspense>
-				<span className="left-100 absolute ml-2">{!!totalItems && totalItems}</span>
-			</Suspense>
+
+			<span className="left-100 absolute ml-2">{!!totalItems && totalItems}</span>
 		</span>
 	);
 	return isCartDisabled ? CartButtonContent : <Link href="/cart">{CartButtonContent}</Link>;
