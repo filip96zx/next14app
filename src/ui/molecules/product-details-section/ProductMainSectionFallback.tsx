1d942d9433e1cc97df24759ba5dd9297132f0fb0
Filip Cudny <filip96zx@gmail.com>
2024-02-03 08:53:00 +0100

    Zmiana tytułu strony

--- src/ui/molecules/product-details-section/ProductMainSectionFallback.tsx
+++ src/ui/molecules/product-details-section/ProductMainSectionFallback.tsx
@@ -1,15 +1,25 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 import { ProductMainSection as ProductMainSection } from "./ProductMainSection";
 import { Spinner } from "@/ui/atoms/Spinner";
 import { useProductCache } from "@/services/product-cache.service";
 import { BackButton } from "@/ui/atoms/buttons";
+import { getMetadataTitle } from "@/utils";
 
 export const ProductMainSectionFallback = () => {
 	const { product } = useProductCache();
 	const pathname = usePathname();
 	const isProductPage = pathname.includes("/product/");
-	return product && isProductPage ? (
+
+	const showProduct = product && isProductPage;
+
+	useEffect(() => {
+		if (showProduct) {
+			document.title = getMetadataTitle(product.name);
+		}
+	}, [product?.name, showProduct]);
+	return showProduct ? (
 		<div className="flex flex-col  items-center justify-center gap-5">
 			<div>
 				<BackButton href={"/products"} keepSearchParams>
