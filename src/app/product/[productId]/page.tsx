72891ecbd6167fa6f9fb68c1ebd879b7ae2375d0
Filip Cudny <filip96zx@gmail.com>
2024-02-03 16:32:03 +0100

    perf: Product details optimization (#10)
    * Przeniesienie obrazków
    
    * Dodanie szczegółów produktu na stronie ładownia
    
    * Zmiana tytułu strony

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -4,14 +4,10 @@ import { Suspense } from "react";
 import { getProductById, getProductRelatedProductsByProductName, getProductsList } from "@/api";
 import { getMetadataTitle } from "@/app/utils";
 import { PaginatedProductList, getPaginationParams } from "@/app/ui/organisms/list";
-import { BackFormerPageParamName } from "@/app/models";
-import { AddToCartButton, BackButton } from "@/app/ui/atoms/buttons";
-import { NumberInput, Select } from "@/app/ui/atoms/inputs";
-import { addToCartServerAction } from "@/app/services/server-actions";
+import { type BackFormerPageParamName } from "@/app/models";
 import { ReviewList } from "@/app/ui/molecules/review/ReviewList";
-import { RatingStarsWithLabel } from "@/app/ui/atoms/rating";
 import { Spinner } from "@/app/ui/atoms/Spinner";
-import { ProductCard } from '@/app/ui/molecules/ProductCard';
+import { ProductDetails } from "@/app/ui/organisms/ProductDetails";
 
 export const generateMetadata = async ({ params }: { params: { productId: string } }) => {
 	const { content } = await getProductsList({});
@@ -50,64 +46,48 @@ interface IProps {
 }
 
 export default async function ProductPage({ params, searchParams }: IProps) {
-	const { [BackFormerPageParamName.FROM]: from, page = 1 } = searchParams;
+	const { page = 1 } = searchParams;
 	const product = await getProductById(params.productId);
 
 	if (!product) {
 		throw notFound();
 	}
+	const variants = product.variants.map((v) => ({ name: v.name, value: v.id }));
 	const collectionSlug = product.collections[0]?.slug;
 
 	return (
-		<div className="flex flex-col  items-center justify-center gap-5">
-			<div>
-				<BackButton href={(from ? `${from}` : "/products") as Route} keepSearchParams>
-					{!from && "All products"}
-				</BackButton>
-			</div>
-			<h1 className="text-center text-2xl font-bold text-gray-800">{product.name}</h1>
-			<div className="max-w-md">
-				<ProductCard product={product} />
-				<RatingStarsWithLabel rating={product.averageRating} ratingCount={product.ratingsCount} />
-				<form action={addToCartServerAction.bind(null, product.id)}>
-					<Select
-						name="variantId"
-						options={product.variants.map((v) => ({ name: v.name, value: v.id }))}
-					/>
-					<NumberInput
-						name="quantity"
-						min={1}
-						max={100}
-						defaultValue={1}
-						className="w-20"
-						setDefaultValueOnClear
-					/>
-					<AddToCartButton />
-				</form>
-			</div>
-			<p className="text-center text-gray-500">{product.description}</p>
-			{collectionSlug && (
-				<Suspense fallback={<Spinner />}>
-					<PaginatedProductList
-						getListQuery={getProductRelatedProductsByProductName}
-						params={{
-							...getPaginationParams({ pageNumber: page, pageSize: 4 }),
-							productName: product.name,
-						}}
-						dataTestid="related-products"
-						// TODO task 1
-						// goBackParams={`/product/${params.productId}${createQueryParams({
-						// 	from,
-						// 	page,
-						// 	...restParams,
-						// })}`}
-						route={`/product/${params.productId}` as Route}
-						searchParamsPagination
-						hidePagination
-					/>
-				</Suspense>
-			)}
-			<ReviewList productId={params.productId} />
-		</div>
+		<ProductDetails
+			product={product}
+			productVariants={variants}
+			afterProductSection={
+				<>
+					{collectionSlug && (
+						<Suspense fallback={<Spinner />}>
+							<PaginatedProductList
+								getListQuery={getProductRelatedProductsByProductName}
+								params={{
+									...getPaginationParams({ pageNumber: page, pageSize: 4 }),
+									productName: product.name,
+								}}
+								dataTestid="related-products"
+								// TODO task 1
+								// goBackParams={`/product/${params.productId}${createQueryParams({
+								// 	from,
+								// 	page,
+								// 	...restParams,
+								// })}`}
+								route={`/product/${params.productId}` as Route}
+								searchParamsPagination
+								hidePagination
+							/>
+						</Suspense>
+					)}
+
+					<Suspense fallback={<Spinner />}>
+						<ReviewList productId={params.productId} />
+					</Suspense>
+				</>
+			}
+		/>
 	);
 }
