19a352949e94654047306228644d2dc8c547ca23
Filip Cudny <filip96zx@gmail.com>
2024-07-04 21:39:04 +0200

    product list page static generation

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -1,26 +1,26 @@
 import { type Metadata } from "next";
 import { getProductsList } from "@/api";
-// import { LIST_PAGE_SIZE } from "@/app/constants";
 import { PageHeader } from "@/ui/atoms/PageHeader";
 import { PaginatedProductList, getPaginationParams } from "@/ui/organisms/list";
 import { getMetadataTitle } from "@/utils";
 import { SortSelect } from "@/ui/atoms/inputs/SortSelect";
 import { type SortOrder, type SortableField } from "@/gql/graphql";
+import { LIST_PAGE_SIZE } from "@/constants";
 
 export const metadata: Metadata = {
 	title: getMetadataTitle("All products"),
 	description: "All products list",
 };
 
-// export async function generateStaticParams() {
-// 	const { totalElements } = await getProductsList({
-// 		first: 1,
-// 		skip: 0,
-// 	});
-// 	return Array.from({ length: Math.ceil(totalElements / LIST_PAGE_SIZE) }, (_, i) => ({
-// 		pageNumber: (i + 1).toString(),
-// 	}));
-// }
+export async function generateStaticParams() {
+	const { totalElements } = await getProductsList({
+		first: 1,
+		skip: 0,
+	});
+	return Array.from({ length: Math.ceil(totalElements / LIST_PAGE_SIZE) }, (_, i) => ({
+		pageNumber: (i + 1).toString(),
+	}));
+}
 
 type TProps = {
 	params: { pageNumber: string };
